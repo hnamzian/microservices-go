2f0a06183188d2cd3da9d9fb63f4b990cbefa4be
hnamzian <namazian87@gmail.com>
2022-10-18 14:34:27 +0300

    Create productsResponse swagger doc

---
+++ product-api/handlers/docs.go
@@ -0,0 +1,26 @@
+// Package classification of Product API
+//
+// Documentation for Product API
+//
+//	Schemes: http
+//	BasePath: /
+//	Version: 1.0.0
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
+package handlers
+
+import "microservices-go/data"
+
+// A list of products
+// swagger:response productsResponse
+type ProductsResponseWrapper struct {
+	// The error message
+	// in: body
+	Body []data.Product
+}
