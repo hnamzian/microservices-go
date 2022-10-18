68c015bef1dc06ab2da4e4ffd7af9e2d16ce0be8
hnamzian <namazian87@gmail.com>
2022-10-18 14:33:57 +0300

    Add docs to Product model

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -1,37 +1,50 @@
 package data
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"regexp"
-	"time"
 
 	"github.com/go-playground/validator"
 )
 
+var ErrorProductNotFound = fmt.Errorf("Product Not Found")
+
+// Product defines the structure for an API product
+// swagger:model Product
 type Product struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name" validate:"required"`
-	Discription string  `json:"description" validate:"required"`
-	Price       float32 `json:"price" validate:"gt=0"`
-	SKU         string  `json:"sku" validate:"sku"`
-	CreatedOn   string  `json:"-"`
-	UpdatedOn   string  `json:"-"`
-	DeletedOn   string  `json:"-"`
+	// the id for the product
+	//
+	// required: false
+	// min: 1
+	ID int `json:"id"` // Unique identifier for the product
+
+	// the name for this poduct
+	//
+	// required: true
+	// max length: 255
+	Name string `json:"name" validate:"required"`
+
+	// the description for this poduct
+	//
+	// required: false
+	// max length: 10000
+	Description string `json:"description"`
+
+	// the price for the product
+	//
+	// required: true
+	// min: 0.01
+	Price float32 `json:"price" validate:"required,gt=0"`
+
+	// the SKU for the product
+	//
+	// required: true
+	// pattern: [a-z]+-[a-z]+-[a-z]+
+	SKU string `json:"sku" validate:"sku"`
 }
 
-func (p *Product) FromJSON(r io.Reader) error {
-	e := json.NewDecoder(r)
-	return e.Decode(p)
-}
 
 type Products []*Product
-
-func (p *Products) ToJSON(w io.Writer) error {
-	e := json.NewEncoder(w)
-	return e.Encode(p)
-}
 
 func (p *Product) ValidateProduct() error {
 	validate := validator.New()
@@ -43,7 +56,7 @@ func (p *Product) ValidateProduct() error {
 func validateSKU(fl validator.FieldLevel) bool {
 	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
 	matches := re.FindAllString(fl.Field().String(), -1)
-	
+
 	return len(matches) == 1
 }
 
@@ -53,8 +66,8 @@ func GetProductList() Products {
 
 func AddProduct(p *Product) {
 	p.ID = getNextId()
-	p.CreatedOn = time.Now().UTC().String()
-	p.UpdatedOn = time.Now().UTC().String()
+	// p.CreatedOn = time.Now().UTC().String()
+	// p.UpdatedOn = time.Now().UTC().String()
 	productList = append(productList, p)
 }
 
@@ -70,12 +83,21 @@ func UpdateProduct(id int, p *Product) error {
 	return nil
 }
 
+func DeleteProduct(id int) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return ErrorProductNotFound
+	}
+
+	productList = append(productList[:pos], productList[pos+1])
+
+	return nil
+}
+
 func getNextId() int {
 	id := productList[len(productList)-1].ID
 	return id + 1
 }
-
-var ErrorProductNotFound = fmt.Errorf("Product Not Found")
 
 func findProduct(id int) (*Product, int, error) {
 	for i, p := range productList {
@@ -90,19 +112,19 @@ var productList = Products{
 	{
 		ID:          1,
 		Name:        "Latte",
-		Discription: "Frothy milky coffee",
+		Description: "Frothy milky coffee",
 		Price:       2.45,
 		SKU:         "abc323",
-		CreatedOn:   time.Now().UTC().String(),
-		UpdatedOn:   time.Now().UTC().String(),
+		// CreatedOn:   time.Now().UTC().String(),
+		// UpdatedOn:   time.Now().UTC().String(),
 	},
 	{
 		ID:          2,
 		Name:        "Espresso",
-		Discription: "Short and strong coffee without milk",
+		Description: "Short and strong coffee without milk",
 		Price:       1.99,
 		SKU:         "fjd34",
-		CreatedOn:   time.Now().UTC().String(),
-		UpdatedOn:   time.Now().UTC().String(),
+		// CreatedOn:   time.Now().UTC().String(),
+		// UpdatedOn:   time.Now().UTC().String(),
 	},
 }
