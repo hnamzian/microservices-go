6b07f77a84bdf16663db84e8fdc2071fd26271da
hnamzian <namazian87@gmail.com>
2022-10-19 10:01:21 +0300

    Create APIs to post and get files

--- product-images/main.go
+++ product-images/main.go
@@ -11,23 +11,41 @@ import (
 	"github.com/gorilla/mux"
 	hclog "github.com/hashicorp/go-hclog"
 	"github.com/hnamzian/microservices-go/product-images/handlers"
+	"github.com/hnamzian/microservices-go/product-images/files"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
+	bindAddress := os.Getenv("BIND_ADDRESS")
+	basePath := os.Getenv("BASE_PATH")
+
 	l := hclog.New(&hclog.LoggerOptions{
 		Name:  "product-images",
 		Level: hclog.LevelFromString("DEBUG"),
 	})
 
-	handlers.NewFiles(l)
+	stor, err := files.NewLocal(basePath, 1024*5*1000)
+	if err != nil {
+		l.Error("Unable to create storage", "error", err)
+		os.Exit(1)
+	}
 
-	fm := mux.NewRouter()
+
+	ph := handlers.NewFiles(stor, l)
+
+	sm := mux.NewRouter()
+	sm.Methods(http.MethodPost).Subrouter().HandleFunc("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", ph.ServeHTTP)
+	sm.Methods(http.MethodGet).Subrouter().Handle(
+		"/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}", 
+		http.StripPrefix("/images", http.FileServer(http.Dir(basePath))))
+
 
 	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
 
 	s := http.Server{
-		Addr:    ":9090",
-		Handler: ch(fm),
+		Addr:    bindAddress,
+		Handler: ch(sm),
 
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
@@ -35,7 +53,7 @@ func main() {
 	}
 
 	go func() {
-		l.Info("Server starts Running on Port 9090")
+		l.Info("Server starts Running", "bindAddress", bindAddress)
 		err := s.ListenAndServe()
 		if err != nil {
 			l.Error("Unable to Start Server", "error", err)
